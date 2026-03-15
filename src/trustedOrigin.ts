a10320654ab959163d28ed993fcc7e2662a7d032
Aayurt <aayurtshrestha@gmail.com>
2026-03-15 14:17:32 +0545

    Changes

--- src/trustedOrigin.ts
+++ src/trustedOrigin.ts
@@ -2,5 +2,6 @@ export const trustedOriginsValues = [
     "http://localhost:5173",
     'capacitor://localhost',   // iOS mobile app
     'http://localhost',
-    "https://events-theta-three.vercel.app"
+    "https://events-theta-three.vercel.app",
+    "127.0.0.1:54971"
 ]
